5f22457c25c56a8c7946bbad9a21396b3dfd6c68
Ryan Marsh <ryanjmarsh93@gmail.com>
2023-01-13 12:17:55 +0000

    Completed Task 9

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,32 @@
-
+import { useState, useEffect } from "react";
+import { fetchTopics } from "../api";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <div>
-            <h1>NC-NEWS</h1>
-        </div>
-    )
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    fetchTopics().then((topics) => {
+      setTopics(topics);
+    });
+  });
+
+  return (
+    <div>
+      <Link to="/">
+        <h1>NC-NEWS</h1>
+      </Link>
+      <ul className="TopicsList">
+        {topics.map((topic) => {
+          return (
+            <li className="TopicsListItem" key={topic.slug}>
+              <Link to={`topics/${topic.slug}`}>{topic.slug}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
